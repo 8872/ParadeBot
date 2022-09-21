7e839870ece894403be85d858b25dfbd71b5da9e
brin <itsbrian1113@gmail.com>
2022-09-20 17:09:17 -0700

    armtest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTest.java
@@ -2,50 +2,32 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp
 public class ArmTest extends LinearOpMode{
-    private DcMotor arm;
-    private boolean auto = false;
+    private DcMotorEx arm;
+    //private boolean auto = false;
 
 
     @Override
     public void runOpMode() throws InterruptedException {
-        arm = hardwareMap.dcMotor.get("hand_motor");
-        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm = hardwareMap.get(DcMotorEx.class, "hand_motor");
+
+
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
         while(opModeIsActive()){
-            telemetry.addData("runmode", auto);
-            telemetry.addData("position", arm.getCurrentPosition());
-            if(gamepad1.a){
-                auto = !auto;
-            }
-            if(auto == true){
-                arm.setPower(-0.5);
-                sleep(1000);
-                arm.setPower(0.5);
-                sleep(1000);
-                /*
-                telemetry.addData("position", arm.getCurrentPosition());
-                if(!arm.isBusy()){
-                    if(arm.getCurrentPosition() > 0){
-                        arm.setTargetPosition(-800);
-                        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    }else{
-                        arm.setTargetPosition(800);
-                        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    }
-                }
-                 */
+            //telemetry.addData("runmode", auto);
 
-            }else{
-                double power = -gamepad1.left_stick_y;
-                arm.setPower(power);
-                telemetry.addData("power", arm.getPower());
-            }
+            double power = -gamepad1.left_stick_y;
+            arm.setPower(power);
+            telemetry.addData("power", arm.getPower());
+            telemetry.addData("position", arm.getCurrentPosition());
+
             telemetry.update();
 
         }
