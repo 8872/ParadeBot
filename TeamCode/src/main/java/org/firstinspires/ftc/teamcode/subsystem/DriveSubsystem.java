958ca8442cfa0aca815d7005a1294c246d914773
wilcoxrobotics <teamrobotics8872@gmail.com>
2022-12-12 15:57:15 -0800

    Doing DriveSystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/DriveSubsystem.java
@@ -2,8 +2,21 @@ package org.firstinspires.ftc.teamcode.subsystem;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 
 public class DriveSubsystem extends SubsystemBase {
 
+    private MecanumDrive drive;
 
+    public DriveSubsystem (MotorEx Fl, MotorEx Fr, MotorEx Bl, MotorEx Br) {
+        drive = new MecanumDrive(Fl, Fr, Bl, Br);
+    }
+
+    public void DriveRobotCentric(double strafeSpeed, double forwardSpeed, double turnSpeed) {
+        drive.driveRobotCentric(strafeSpeed, forwardSpeed, turnSpeed);
+    }
+
+    public void DriveFieldCentric(double strafeSpeed, double forwardSpeed, double turn, double heading) {
+        drive.driveFieldCentric(strafeSpeed, forwardSpeed, turn, heading);
+    }
 }
